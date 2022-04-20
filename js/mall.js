49b1edb3cc1c23bf38b234c4b7fe445977c50c54
EunHeeChoi1955 <med20001955@naver.com>
2022-04-20 17:14:21 +0900

    220420

--- js/mall.js
+++ js/mall.js
@@ -14,6 +14,7 @@
             this.section6Fn();
             this.section8BtnFn();
             this.section9Fn();
+            this.section10Fn();
             this.footerFn();
           
     
@@ -332,7 +333,7 @@
             let   swipeEnd       = null;
             let   dragStart      = null; // 슬라이드 마지막이 처음에서 왼쪽으로 이동된 상태 값을 빼주고 시작
             let   mouseDown      = null; // 반드시 마우스가 다우된 상태를판단 다운이면 tru, 업이면 false
-
+            var   heart          = $('#section3 .heart');
 
            
 
@@ -416,6 +417,22 @@
                }
             });
 
+
+
+            
+               // heart 클릭 이벤트
+               heart.each(function (idx) {
+                  $(this).on({
+                      click: function () {
+                      
+                        heart.eq(idx).toggleClass('addHeart'); // 클래스 주입
+                         
+                      }
+                  });
+              });
+
+
+
         },
         section5Fn:  function(){
 
@@ -426,7 +443,7 @@
          let   swipeEnd       = null;
          let   dragStart      = null; // 슬라이드 마지막이 처음에서 왼쪽으로 이동된 상태 값을 빼주고 시작
          let   mouseDown      = null; // 반드시 마우스가 다우된 상태를판단 다운이면 tru, 업이면 false
-
+         var   heart          = $('#section5 .heart');
 
         
 
@@ -511,6 +528,19 @@
                   }   
             }
          });
+
+
+         
+               // heart 클릭 이벤트
+               heart.each(function (idx) {
+                  $(this).on({
+                      click: function () {
+                      
+                        heart.eq(idx).toggleClass('addHeart'); // 클래스 주입
+                         
+                      }
+                  });
+              });
 
         },
         section6Fn:  function(){
@@ -729,13 +759,13 @@
          var setId = 0;
          var t = 0;
          var t2 = 0;
-         var ratio = $('.item').eq(0).innerWidth();
+         var ratio = $(' #section9 .item').eq(0).innerWidth();
          var slideW = $(window).innerWidth()*( ratio / $(window).innerWidth() ); //슬라이드너비 = 창너비*(슬라이드너비/창너비); //0.435627956 슬라이드 1개의 창너비의 대한 너비 비율
-         var m = $('.screensWrap>li').length+1; //총 슬라이드 갯수 좌측슬라이드+중앙슬라이드+우측슬라이드
+         var m = $('#section9 .screensWrap>li').length+1; //총 슬라이드 갯수 좌측슬라이드+중앙슬라이드+우측슬라이드
          var n = m-2-1; //가운데 중앙슬라이드 갯수 4 = 7-2-1 (슬라이드는 5개 인덱스번호는 0 ~ 4)
          //var slideW = $(window).innerWidth()*ratio; //슬라이드너비 = 창너비*슬라이드1개의비율값
          var cnt = 0;
-         var heart = $('.heart');
+         var heart = $('#section9 .heart');
         
          //let   cnt            = 0;
          let   swipeStart     = null;
@@ -759,11 +789,12 @@
             
             //슬라이드의 모든 버튼위에 마우스 올리면 타이머 일시정지
             
-            $('.slideControl').on({
+            $('#section9 .slideControl, .screensWrap').on({
                
                mouseover:	function(e){
                   clearInterval(setId);  	//일시정지
                   $(e.target).addClass('addBtn9');
+                  
                   t=0;
                },
                mouseout:	function(e){				
@@ -772,6 +803,7 @@
                      clearInterval(setId);  	//일시정지
                      autoPlay('_mouseleave'); //플레이(||)	상태에서 자동 실행 가능	
                      $(e.target).removeClass('addBtn9');
+                     
                   }
                }
                
@@ -784,10 +816,10 @@
             //메인 슬라이드 함수
             function mainSlide(){
             
-               $('.screensWrap').stop().animate({ left: -slideW*cnt}, 600, function(){
+               $('#section9 .screensWrap').stop().animate({ left: -slideW*cnt}, 600, function(){
                   cnt>n?cnt=0:cnt;
                   cnt<0?cnt=n:cnt;
-                  $('.screensWrap').stop().animate({ left: -slideW*cnt}, 0);
+                  $('#section9 .screensWrap').stop().animate({ left: -slideW*cnt}, 0);
                   cnt>5?cnt=0:cnt;
                });
             
@@ -799,10 +831,10 @@
             //네비게이션 버튼(페이지 버튼==인디게이터 버튼) 마킹(Marking) 이벤트 
             function navFn(){
                
-               $('.navItems').removeClass('addBtn9');	    //초기화
-               $('.item').removeClass('addBtn9');	    //초기화
-               $('.navItems').eq(cnt>n?0:cnt).addClass('addBtn9');	//해당 슬라이드 버튼만 표시 
-               $('.item').eq(cnt>n?0:cnt).addClass('addBtn9');	//해당 슬라이드 버튼만 표시 
+               $('#section9 .navItems').removeClass('addBtn9');	    //초기화
+               $('#section9 .item').removeClass('addBtn9');	    //초기화
+               $('#section9 .navItems').eq(cnt>n?0:cnt).addClass('addBtn9');	//해당 슬라이드 버튼만 표시 
+               $('#section9 .item').eq(cnt>n?0:cnt).addClass('addBtn9');	//해당 슬라이드 버튼만 표시 
       
                   
                      
@@ -813,7 +845,7 @@
             ////////////////////////////////////////////////////////////////////////
             //네비게이션 버튼 클릭이벤트
             //네비게이션 버튼을 배열처리(each()메소드)하여 인덱스값 이용
-            $('.navItems').each(function(index){
+            $('#section9 .navItems').each(function(index){
                $(this).on({
                   click:	function(){
                      cnt = index;
@@ -898,37 +930,293 @@
 
 
 
+
          // heart 클릭 이벤트
-            heart.on({
-               click:  function(event){
-                     event.preventDefault();
-                     $this = $(this); //현재 클릭한 이(this) 버튼
-                     heart.toggleClass('addHeart');
-                     
-              
-            }
-         });
+         heart.each(function (idx) {
+            $(this).on({
+                click: function () {
+                
+                  heart.eq(idx).toggleClass('addHeart'); // 클래스 주입
+                   
+                }
+            });
+        });
+
+       
+
+        
 
         },
+        section10Fn: function(){
+
+         // const slideWrap      = $('#section10 .slide-wrap');
+         // const slideContainer = $('#section10 .slide-view');
+         // var setId = 0;
+         // var t = 0;
+         // var t2 = 0;
+         // var ratio = $(' #section10 .item').eq(0).innerHeight();
+         // var slideH = $(window).innerHeight()*( ratio / $(window).innerHeight() ); //슬라이드너비 = 창너비*(슬라이드너비/창너비); //0.435627956 슬라이드 1개의 창너비의 대한 너비 비율
+         // var m = $('#section10 .screensWrap>li').length+1; //총 슬라이드 갯수 좌측슬라이드+중앙슬라이드+우측슬라이드
+         // var n = m-1-1; //가운데 중앙슬라이드 갯수 4 = 7-2-1 (슬라이드는 5개 인덱스번호는 0 ~ 4)
+         // //var slideW = $(window).innerWidth()*ratio; //슬라이드너비 = 창너비*슬라이드1개의비율값
+         // var cnt = 0;
+      
+            
+         //    autoPlay('_play');  //자동실행 4초 후에 실행
+         
+         
+         //    function autoPlay(z){
+         //       if( z=='_play' || z=='_mouseleave' || z=='_play_auto'){
+         //          if(t2==0){ //플레이버튼 클릭과 마우스떠났을때
+         //                   //두번 자동실행을 막기 위해 t2변수 사용 버블링 디버깅.
+                           
+         //             setId = setInterval(nextSlide,3000);
+         //             t2=1;
+         //          }
+         //       }			
+         //    }	
+            
+            
+         //    //슬라이드의 모든 버튼위에 마우스 올리면 타이머 일시정지
+            
+         //    $('#section10 .slideControl, .screensWrap').on({
+               
+         //       mouseover:	function(e){
+         //          clearInterval(setId);  	//일시정지
+         //          $(e.target).addClass('addBtn10');
+         //          t=0;
+         //       },
+         //       mouseout:	function(e){				
+         //          if(t==0){  	//플레이버튼 스톱(▶) 상태이라면 플레이 불가능
+         //             t2=0;	//자동실행함수를 호출시 변수 초기화
+         //             clearInterval(setId);  	//일시정지
+         //             autoPlay('_mouseleave'); //플레이(||)	상태에서 자동 실행 가능	
+         //             $(e.target).removeClass('addBtn10');
+         //          }
+         //       }
+               
+         //    });
+      
+         
+         
+          
+      
+         
+         //    //메인 슬라이드 함수
+         //    function mainSlide(){
+            
+         //       $('#section10 .screensWrap').stop().animate({ top: -slideH*cnt}, 600, function(){
+         //          cnt>n?cnt=0:cnt;
+         //          cnt<0?cnt=n:cnt;
+         //          $('#section10 .screensWrap').stop().animate({ top: -slideH*cnt}, 0);
+         //          cnt>5?cnt=0:cnt;
+         //       });
+            
+         //       navFn();
+               
+         //    }
+            
+      
+         //    //네비게이션 버튼(페이지 버튼==인디게이터 버튼) 마킹(Marking) 이벤트 
+         //    function navFn(){
+               
+         //       $('#section10 .navItems').removeClass('addBtn10');	    //초기화
+         //       $('#section10 .item').removeClass('addBtn10');	    //초기화
+         //       $('#section10 .navItems').eq(cnt>n?0:cnt).addClass('addBtn10');	//해당 슬라이드 버튼만 표시 
+         //       $('#section10 .item').eq(cnt>n?0:cnt).addClass('addBtn10');	//해당 슬라이드 버튼만 표시 
+      
+                  
+                     
+         //    }		
+      
+         
+      
+         //    ////////////////////////////////////////////////////////////////////////
+         //    //네비게이션 버튼 클릭이벤트
+         //    //네비게이션 버튼을 배열처리(each()메소드)하여 인덱스값 이용
+         //    $('#section10 .navItems').each(function(index){
+         //       $(this).on({
+         //          click:	function(){
+         //             cnt = index;
+         //             mainSlide();
+         //          }
+         //       });
+         //    });
+      
+      
+         //    //다음 슬라이드 카운트 함수
+         //    function nextSlide(){
+         //       cnt++;
+         //       mainSlide();
+         //    }
+      
+      
+         //    //이전 슬라이드 카운트 함수
+         //    function prevSlide(){
+         //       cnt--;
+         //       mainSlide();
+         //    }
+      
+      
+         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+         const slideWrap      = $('#section10 .slide-wrap');
+          //const slideContainer = $('#section10 .slide-view');
+          var setId = 0;
+          var t = 0;
+          var t2 = 0;
+          //var ratio = $(' #section10 .item').eq(0).innerHeight();
+          //var slideH = $(window).innerHeight()*( ratio / $(window).innerHeight() ); //슬라이드너비 = 창너비*(슬라이드너비/창너비); //0.435627956 슬라이드 1개의 창너비의 대한 너비 비율
+          var m = $('#section10 .img-box'); //총 슬라이드 갯수 좌측슬라이드+중앙슬라이드+우측슬라이드
+          var n = m-1; //가운데 중앙슬라이드 갯수 4 = 7-2-1 (슬라이드는 5개 인덱스번호는 0 ~ 4)
+         // var slideH = $(window).innerHeight()*ratio; //슬라이드너비 = 창너비*슬라이드1개의비율값
+          var cnt = 0;
+          var Btn =$('#section10 .pageBtn ');
+          var item =$('#section10 .item');
+        
+
+            
+             autoPlay('_play');  //자동실행 4초 후에 실행
+         
+         
+            function autoPlay(z){
+               if( z=='_play' || z=='_mouseleave' || z=='_play_auto'){
+                  if(t2==0){ //마우스떠났을때
+                            //두번 자동실행을 막기 위해 t2변수 사용 버블링 디버깅.
+                           
+                     setId = setInterval(nextSlide,4000);
+                      t2=1;
+                   }
+                }			
+             }	
+            
+            
+             //슬라이드의 모든 버튼위에 마우스 올리면 타이머 일시정지
+             $('#section10 .screensWrap').on({
+               
+                mouseover:	function(e){
+                   clearInterval(setId);  	//일시정지
+                   $(e.target).addClass('addSec10');
+                   t=0;
+                },
+                mouseout:	function(e){				
+                   if(t==0){  	//플레이버튼 스톱(▶) 상태이라면 플레이 불가능
+                      t2=0;	//자동실행함수를 호출시 변수 초기화
+                      clearInterval(setId);  	//일시정지
+                      autoPlay('_mouseleave'); //플레이(||)	상태에서 자동 실행 가능	
+                      $(e.target).removeClass('addSec10');
+                   }
+                }
+               
+             });
+      
+         
+         
+          
+      
+             //메인 슬라이드 함수
+             function mainSlide(){
+            
+                $('#section10 .img-box').eq(cnt>n?0:cnt).stop().slideDown(600, function(){  
+                   $('#section10 .img-box').eq(cnt>n?0:cnt).delay(1000).slideUp(400);
+               });
+            
+               navFn();
+               
+            }
+            
+      
+             //네비게이션 버튼(페이지 버튼==인디게이터 버튼) 마킹(Marking) 이벤트 
+             function navFn(){
+               Btn.eq(cnt>n?0:cnt).addClass('addSec10');	//이거 여기다 둬야  슬라이드 1번 버튼 addclass 작용
+               Btn.removeClass('addSec10');	    //초기화
+               item.removeClass('addSec10');	    //초기화
+               cnt>4?cnt=0:cnt;
+                Btn.eq(cnt>n?0:cnt).addClass('addSec10');	//해당 슬라이드 버튼만 표시 
+                item.eq(cnt>n?0:cnt).addClass('addSec10');	//해당 슬라이드 버튼만 표시 
+                
+                  
+                     
+             }		
+      
+         
+      
+             ////////////////////////////////////////////////////////////////////////
+             //네비게이션 버튼 클릭이벤트
+             //네비게이션 버튼을 배열처리(each()메소드)하여 인덱스값 이용
+             Btn.each(function(index){
+                $(this).on({
+                   click:	function(){
+                      cnt = index;
+                      mainSlide();
+                   }
+                });
+             });
+      
+      
+             //다음 슬라이드 카운트 함수
+             function nextSlide(){
+                cnt++;
+               mainSlide();
+             }
+      
+      
+            //이전 슬라이드 카운트 함수
+           function prevSlide(){
+               cnt--;
+              mainSlide();
+           }
+      
+
+
+
+
+        
+
+        }, 
         footerFn:  function(){
-         var copyBtn =  document.getElementById('copy');
-         copyBtn.addEventListener("click", function(){
-            // 복사할 텍스트를 변수에 할당해줍니다.
-             var text = document.getElementById("textDiv").innerText;
-             // input text 태그를 생성해줍니다.
-             var createInput = document.createElement("input");
-             createInput.setAttribute("type", "text");
-             // 가상으로 가져올 태그에 만들어준 input 태그를 붙여줍니다.
-             document.getElementById("textDiv").appendChild(createInput);
-             // 만든 input 태그의 value 값에 복사할 텍스트 값을 넣어줍니다.
-             createInput.value = text;
-             // 복사 기능을 수행한 후
-             createInput.select();
-             document.execCommand('copy');
-             // 가상으로 붙여주었던 input 태그를 제거해줍니다.
-             document.getElementById("textDiv").removeChild(createInput);
-             alert('복사가 완료되었습니다.');
-         });
+           
+            var copyBtn =  document.getElementById('copy');
+
+
+
+            copyBtn.addEventListener("click", function(){
+               // 복사할 텍스트를 변수에 할당해줍니다.
+               var text = document.getElementById("textDiv").innerText;
+               // input text 태그를 생성해줍니다.
+               var createInput = document.createElement("input");
+               createInput.setAttribute("type", "text");
+               // 가상으로 가져올 태그에 만들어준 input 태그를 붙여줍니다.
+               document.getElementById("textDiv").appendChild(createInput);
+               // 만든 input 태그의 value 값에 복사할 텍스트 값을 넣어줍니다.
+               createInput.value = text;
+               // 복사 기능을 수행한 후
+               createInput.select();
+               document.execCommand('copy');
+               // 가상으로 붙여주었던 input 태그를 제거해줍니다.
+               document.getElementById("textDiv").removeChild(createInput);
+               alert('복사가 완료되었습니다.');
+            });
    
         
         }
